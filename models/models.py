a0ceb80ea0889f374415360711c06cc215fa774e
hampet1 <46238335+hampet1@users.noreply.github.com>
2021-10-11 12:05:49 +1100

    arma model updated

--- models/models.py
+++ models/models.py
@@ -1,12 +1,11 @@
 import pandas as pd
-from statsmodels.tsa.arima_model import ARIMA
+import statsmodels.tsa.arima.model as stats
 
 
 
 def arma_model(train, type_of_model, days_forecast):
     """
     by default only second degree order (order = 2) in order to make it easier for the users
-
     """
     order_AR = 0
     order_MA = 0
@@ -22,24 +21,20 @@ def arma_model(train, type_of_model, days_forecast):
     else:
         return "wrong data type or wrong term, the only correct arguments are AR, MA or ARMA"
 
-    model_arma = ARIMA(train['Log return'], order=(order_AR, 0, order_MA))
+    model_arma = stats.ARIMA(train, order=(order_AR, 0, order_MA))
     results_ar = model_arma.fit()
     # forecasting
     pred = results_ar.forecast(days_forecast)
 
-    predictions = pred[0]
-    # lower bound for all tree predictions
-    lower_bound = pred[2][:, 0]
-    # upper bound for all tree prediction
-    upper_bound = pred[2][:, 1]
     # todays date
     today_date = str(train.index[-1].year) + '-' + str(train.index[-1].month) + '-' + str(train.index[-1].day)
     # future predicted business dates
-
     s = pd.date_range(today_date, periods=days_forecast + 1, freq='b')[1:]
 
-    df = pd.DataFrame(predictions, columns=['Log return'], index=s)
-    output_data = train[['Log return']]
+    df = pd.DataFrame(pred)
+    df = df.rename(columns={"predicted_mean": "Adj Close"})
+
+    output_data = train[["Adj Close"]]
     output_data = output_data.append(df, ignore_index=False)
 
     return output_data
